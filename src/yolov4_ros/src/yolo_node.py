85918d4ac1faaad4a7beeca200f8edfaafe48e54
292213017 <18859275929@163.com>
2021-12-10 22:27:54 +0800

    fix

--- src/yolov4_ros/src/yolo_node.py
+++ src/yolov4_ros/src/yolo_node.py
@@ -13,12 +13,12 @@ import yolo_detect
 
 
 class App:
-    def __init__(self,net, _enable_floating_window, _enable_rate_info):
+    def __init__(self,net):
         rospy.init_node('yolo_detector', log_level=rospy.INFO)
         self.bridge = CvBridge()
         self.detector = net
-        self.enable_rate_info = _enable_rate_info
-        self.enable_floating_window = _enable_floating_window
+        # self.enable_rate_info = _enable_rate_info
+        # self.enable_floating_window = _enable_floating_window
         self.pre_time = rospy.Time.now()
         self.pre_rate = 0.0
         self.r = rospy.Rate(10)
@@ -43,18 +43,18 @@ class App:
             rospy.logwarn("Cannot convert ros image to cv image, err: {}".format(e))
             return
         cv_image = self.detector.realtime_detect(cv_image)
-        if self.enable_rate_info:
-            time_now = rospy.Time.now()
-            time_interval = (time_now - self.pre_time).to_sec()
-            if time_interval > 0:
-                rate = 1.0 / time_interval
-                rate = (rate + self.pre_rate) / 2.0
-            else:
-                rate = 0.0
-            self.pre_rate = rate
-            cv2.putText(cv_image, "FPS {:.2f}".format(rate), (0, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
-        if self.enable_floating_window:
-            cv2.imshow('image-detected', cv_image)
+        # if self.enable_rate_info:
+        #     time_now = rospy.Time.now()
+        #     time_interval = (time_now - self.pre_time).to_sec()
+        #     if time_interval > 0:
+        #         rate = 1.0 / time_interval
+        #         rate = (rate + self.pre_rate) / 2.0
+        #     else:
+        #         rate = 0.0
+        #     self.pre_rate = rate
+        #     cv2.putText(cv_image, "FPS {:.2f}".format(rate), (0, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+        # if self.enable_floating_window:
+        #     cv2.imshow('image-detected', cv_image)
         
         ros_image = self.bridge.cv2_to_imgmsg(cv_image)
         ros_image.header.frame_id = "camera/front"
@@ -79,8 +79,8 @@ class App:
         return exc_type, exc_val, exc_tb
 
 if __name__ == "__main__":
-    enable_floating_window = rospy.get_param("~enable_floating_window", default=False)
-    enable_rate_info = rospy.get_param("~enable_rate_info", default=True)
+    # enable_floating_window = rospy.get_param("~enable_floating_window", default=True)
+    # enable_rate_info = rospy.get_param("~enable_rate_info", default=True)
     sub_image_topic = rospy.get_param("~input_image_topic", default="/d400/color/image_raw")
     cfgfile = rospy.get_param("~model_file", default="/home/autoware/shared_dir/workspace/yolo_ws/src/yolov4_ros/models/yolov4.cfg")
     weightfile = rospy.get_param("~prototxt_file", default="/home/autoware/shared_dir/workspace/yolo_ws/src/yolov4_ros/models/yolov4.weights")
@@ -89,6 +89,6 @@ if __name__ == "__main__":
 
     model = yolo_detect.Detector(cfgfile,weightfile,namesfile,use_cuda)
 
-    app = App(model,enable_floating_window,enable_rate_info)
+    app = App(model)
     app.run()
     
